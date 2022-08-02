313a9e8de10c6e1b7b35acb1f7f704ff7a145324
joseph-roy <110443547+joseph-roy@users.noreply.github.com>
2022-08-02 14:31:01 +1000

    Update script.js
    updated the duration

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ import { check, sleep } from 'k6';
 export const options = {
     stages: [
         { duration: '30s', target: 20 },
-        { duration: '1m30s', target: 10 },
+        { duration: '30s', target: 10 },
         { duration: '20s', target: 0 },
     ],
 };
